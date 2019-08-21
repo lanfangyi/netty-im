387a2edf4d1f6dd3297a7158cc30f834ba17e8d5
lanfangyi <1069344911@qq.com>
2019-08-21 23:07:39 +0800

    no message

---
+++ src/main/java/com/lanfangyi/nettyim/service/UserService.java
@@ -0,0 +1,11 @@
+package com.lanfangyi.nettyim.service;
+
+/**
+ * @author lanfangyi@haodf.com
+ * @version 1.0
+ * @since 2019/8/21 11:06 PM
+ */
+public interface UserService {
+
+    
+}
---
+++ src/main/java/com/lanfangyi/nettyim/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.lanfangyi.nettyim.service.impl;
+
+import com.lanfangyi.nettyim.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lanfangyi@haodf.com
+ * @version 1.0
+ * @since 2019/8/21 11:06 PM
+ */
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+
+}
