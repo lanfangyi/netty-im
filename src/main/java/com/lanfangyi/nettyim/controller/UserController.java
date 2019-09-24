1d8cca1e5f4beccce0f174d3c50d2f812f85f289
lanfangyi <1069344911@qq.com>
2019-09-24 17:16:13 +0800

    no message

--- src/main/java/com/lanfangyi/nettyim/controller/UserController.java
+++ src/main/java/com/lanfangyi/nettyim/controller/UserController.java
@@ -52,4 +52,9 @@ public class UserController {
         return IMResponse.success(user);
     }
 
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(2147483647);
+    }
+
 }
